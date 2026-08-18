fd21537ed7f6a66ca467755dd8b6d56854dbfba9
RabeloTh <thauanrabelosodre@hotmail.com>
2026-08-18 20:14:07 -0300

    Fixação 2

---
+++ exemplosCSharp/Program2.cs
@@ -0,0 +1,5 @@
+﻿using exemplosCSharp.fixacao;
+
+Console.WriteLine("Informe o salário atual:");
+Console.WriteLine("Informe a porcentagem de aumento:");
+
---
+++ exemplosCSharp/fixacao/CalculaAumento.cs
@@ -0,0 +1,11 @@
+﻿
+namespace exemplosCSharp.fixacao
+{
+    internal class CalculaAumento
+    {
+        public static int CalcularAumento(int salario, int porcentagem)
+        {
+            return salario + (salario * porcentagem / 100);
+        }
+    }
+}
